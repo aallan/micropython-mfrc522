0eb89539221374d86ab027880bbac24bdf571edf
Cefn Hoile <github.com@cefn.com>
2017-05-01 18:48:42 +0100

    Tested version of read with new SPI constructor logic

--- examples/read.py
+++ examples/read.py
@@ -1,46 +1,39 @@
 import mfrc522
-from os import uname
+from machine import Pin,SPI
 
 
 def do_read():
+    sck =  Pin(14, mode=Pin.OUT)  # labelled 5 on nodeMCU
+    mosi = Pin(13, mode=Pin.OUT)  # labelled 7 on nodeMCU
+    miso = Pin(12, mode=Pin.IN)  # labelled 6 on nodeMCU
+    spi = SPI(-1, baudrate=100000, polarity=0, phase=0, sck=sck, mosi=mosi, miso=miso)
+    spi.init()
+    rdr = mfrc522.MFRC522(spi=spi, gpioRst=0, gpioCs=2)
 
-	if uname()[0] == 'WiPy':
-		rdr = mfrc522.MFRC522("GP14", "GP16", "GP15", "GP22", "GP17")
-	elif uname()[0] == 'esp8266':
-		rdr = mfrc522.MFRC522(0, 2, 4, 5, 14)
-	else:
-		raise RuntimeError("Unsupported platform")
+    while True:
+        print("Place card")
 
-	print("")
-	print("Place card before reader to read from address 0x08")
-	print("")
+        (stat, tag_type) = rdr.request(rdr.REQIDL)
 
-	try:
-		while True:
+        if stat == rdr.OK:
 
-			(stat, tag_type) = rdr.request(rdr.REQIDL)
+            (stat, raw_uid) = rdr.anticoll()
 
-			if stat == rdr.OK:
+            if stat == rdr.OK:
+                print("Detected")
+                print("type: 0x%02x" % tag_type)
+                print("uid: 0x%02x%02x%02x%02x" % (raw_uid[0], raw_uid[1], raw_uid[2], raw_uid[3]))
+                print("")
 
-				(stat, raw_uid) = rdr.anticoll()
+                if rdr.select_tag(raw_uid) == rdr.OK:
 
-				if stat == rdr.OK:
-					print("New card detected")
-					print("  - tag type: 0x%02x" % tag_type)
-					print("  - uid	 : 0x%02x%02x%02x%02x" % (raw_uid[0], raw_uid[1], raw_uid[2], raw_uid[3]))
-					print("")
+                    key = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
 
-					if rdr.select_tag(raw_uid) == rdr.OK:
-
-						key = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
-
-						if rdr.auth(rdr.AUTHENT1A, 8, key, raw_uid) == rdr.OK:
-							print("Address 8 data: %s" % rdr.read(8))
-							rdr.stop_crypto1()
-						else:
-							print("Authentication error")
-					else:
-						print("Failed to select tag")
-
-	except KeyboardInterrupt:
-		print("Bye")
+                    for sector in range(1, 64):
+                        if rdr.auth(rdr.AUTHENT1A, sector, key, raw_uid) == rdr.OK:
+                            print("data@%d: %s" % (sector, rdr.read(sector)))
+                        else:
+                            print("Auth err")
+                    rdr.stop_crypto1()
+                else:
+                    print("Select failed")
