5965760f2dd5f23423276537190ee5b8c295e942
Cefn Hoile <github.com@cefn.com>
2017-05-04 09:05:49 +0100

    Switched write example to use static literal bytes instead of list for sector key

--- examples/write.py
+++ examples/write.py
@@ -28,7 +28,7 @@ def do_write():
 
                     if rdr.select_tag(raw_uid) == rdr.OK:
 
-                        key = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
+                        key = b'\xff\xff\xff\xff\xff\xff'
 
                         if rdr.auth(rdr.AUTHENT1A, 8, key, raw_uid) == rdr.OK:
                             stat = rdr.write(8, b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f")
